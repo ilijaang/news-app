a96583cb445b3cf4f814264822cb02414373e240
ilijaangeloski <ilija.angeloski@gmail.com>
2024-07-16 23:29:22 +0200

    changed name three

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello Hello test branch two $name!",
+        text = "Hello Hello test branch three $name!",
         modifier = modifier
     )
 }
